ba1fc73e9d7a214356e65a6f7ba079ff976d4360
Leticia Cosin <leticia.cosin@hotmail.com>
2020-07-24 13:26:35 -0300

    egg with countdown in console

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTime = ["Soft": 5, "Medium": 8, "Hard": 12 ]
+    
+    @IBAction func eggButton(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        countdown(qtdEgg: eggTime[hardness]!)
+    }
+    
+    func countdown(qtdEgg: Int){
+        let minutes = qtdEgg * 60
+        var secondsRemaining = minutes
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            secondsRemaining = secondsRemaining - 1
+            print(secondsRemaining)
+          }
+    }
 }

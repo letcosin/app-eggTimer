d909e638a23501bcd48f4f4df917a7a8cb26bce7
Leticia Cosin <leticia.cosin@hotmail.com>
2020-07-24 18:07:31 -0300

    barra de progresso e som funcionando

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,32 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let eggTime = ["Soft": 5, "Medium": 8, "Hard": 12 ]
-    var secondsRemaining = 60
+    var totalTime = 0
+    var secondsRemaining = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var barProgress: UIProgressView!
     
     @IBAction func eggButton(_ sender: UIButton) {
         timer.invalidate()
-        barProgress.progress = 1.0
+        
         let hardness = sender.currentTitle!
-        secondsRemaining = eggTime[hardness]!
+        totalTime = eggTime[hardness]!
+        
+        barProgress.progress = 0
+        secondsRemaining = 0
+        
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsRemaining < totalTime {
+            secondsRemaining += 1
+            let percentProgress = Float(secondsRemaining) / Float(totalTime)
+            barProgress.progress = percentProgress
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            playSound()
         }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
